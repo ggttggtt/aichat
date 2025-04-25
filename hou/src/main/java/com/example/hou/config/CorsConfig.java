f9c99b01a95505dea8d5c284a8b4771e90c19cc6
代刘斌 <3165114700@qq.com>
2025-04-25 10:12:58 +0800

    新建干净项目

---
+++ hou/src/main/java/com/example/hou/config/CorsConfig.java
@@ -0,0 +1,32 @@
+package com.example.hou.config;
+
+/**
+ * @Auther: 代刘斌
+ * @Date: 2023/6/3 - 06 - 03 - 23:04
+ * @Description: com.example.springboot.config
+ * @version: 1.0
+ */
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+public class CorsConfig {
+
+    // 当前跨域请求最大有效时长。这里默认1天
+    private static final long MAX_AGE = 24 * 60 * 60;
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://localhost:8080"); // 1 设置访问源地址
+        corsConfiguration.addAllowedHeader("*"); // 2 设置访问源请求头
+        corsConfiguration.addAllowedMethod("*"); // 3 设置访问源请求方法
+        corsConfiguration.setMaxAge(MAX_AGE);
+        source.registerCorsConfiguration("/**", corsConfiguration); // 4 对接口配置跨域设置
+        return new CorsFilter(source);
+    }
+}
---
+++ hou/vue/src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <div id="app" style="background-color: #FFFFE0">
+    <router-view/>
+  </div>
+</template>
